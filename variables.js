0aa643e197899939f0626255812982a373bc5e03
legndery <ar_itra@yahoo.com>
2017-08-23 16:31:35 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,44 @@
+//var scope
+
+//doesn't care about blocks
+console.log("--Var--")
+var v = "Hello Out function "
+var m = function(){
+    var v = "hello in function"
+    if(1+1 == 2){
+        var v = "Hello in block"
+        console.log("Inside Block:"+ v);
+    }
+    console.log("Outside Block:"+ v);
+}
+m();
+console.log("Out Function:"+v);
+//////////////////////////
+//let's use let
+console.log("--Let--")
+let l = "Hello Out function "
+var n = function(){
+    let l = "hello in function"
+    if(1+1 == 2){
+        let l = "Hello in block"
+        console.log("Inside Block:"+ l);
+    }
+    console.log("Outside Block:"+ l);
+}
+// l = "changed"// no error
+n();
+console.log("Out Function:"+l);
+///const??????????
+console.log("--Const--")
+const c = "Hello Out function "
+var n = function(){
+    const c = "hello in function"
+    if(1+1 == 2){
+        const l = "Hello in block"
+        console.log("Inside Block:"+ c);
+    }
+    console.log("Outside Block:"+ c);
+}
+//c= "Changed" //error
+n();
+console.log("Out Function:"+c);
