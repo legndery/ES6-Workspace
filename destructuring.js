2eba3fdcebad97f03b329fbb7e92815fecf6e2b8
legndery <ar_itra@yahoo.com>
2017-08-23 16:18:59 +0530

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+// array es6 destructuring
+var arr = ["foo", "bar", "baz"]
+
+var [foo, bar, baz] = arr;
+console.log(foo, bar, baz)
+
+//object destucturing
+var obj = {
+    "a":"Hello",
+    "b": "World"
+}
+var {a,b} = obj
+console.log(a,b)
